2735df49058fec0efa9ff87f038551542fcec0ee
Inspecode <inspecode-bot@rocro.com>
2020-11-26 04:27:32 +0000

    Fix PHP-CS-Fixer errors

--- routes/console.php
+++ routes/console.php
@@ -14,7 +14,8 @@ use Illuminate\Foundation\Inspiring;
 */
 
 Artisan::command(
-    'inspire', function () {
+    'inspire',
+    function () {
         $this->comment(Inspiring::quote());
     }
 )->describe('Display an inspiring quote');
